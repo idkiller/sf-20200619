c47f83113c78ba750ac540bd16a50c5ac924103d
pius lee <idkiller@gmail.com>
2022-05-11 03:15:59 +0900

    set static build setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+const dev = process.env.NODE_ENV === 'development';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		paths: {
+			base: dev ? '' : '/sf-20200619',
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			precompress: false
+		}),
+		prerender: {
+			default: true
+		},
+		trailingSlash: 'always'
 	}
 };
 
